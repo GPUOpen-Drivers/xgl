a9add77c4d45e67c085df65f42d8c1a8e2c6b8db
Constantine Tarasenkov <constantine@protonmail.com>
2017-12-24 00:13:47 +0300

    Fix issue https://github.com/GPUOpen-Drivers/xgl/issues/1

--- icd/api/include/vk_physical_device.h
+++ icd/api/include/vk_physical_device.h
@@ -135,7 +135,7 @@ public:
     VK_INLINE bool GetVkTypeIndexFromPalHeap(Pal::GpuHeap heapIndex, uint32_t* pVkIndex) const
     {
         VK_ASSERT(heapIndex < Pal::GpuHeapCount);
-        VK_ASSERT(pVkIndex);
+        VK_ASSERT(pVkIndex != NULL);
         *pVkIndex = m_memoryPalHeapToVkIndex[static_cast<uint32_t>(heapIndex)];
         if (*pVkIndex >= Pal::GpuHeapCount)
         {
